5532977f0df233cefc67c88d415cbc1d20f0ef41
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-12-18 19:49:42 -0300

    Aula03

---
+++ build/main.js
@@ -0,0 +1,6 @@
+"use strict";
+const nome = "Kauã";
+const canal = "CFB Cursos";
+const curso = "Typescript";
+console.log(nome);
+console.log(curso);
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+const nome="Kauã"
+const canal="CFB Cursos"
+const curso="Typescript"
+
+console.log(nome)
+console.log(curso)
