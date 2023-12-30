3fed272398b62c61f41a503d21f6894c02e89475
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-12-30 15:28:15 -0300

    Aula20

--- build/main.js
+++ build/main.js
@@ -1,42 +1,13 @@
 "use strict";
-class Computador {
-    nome;
-    ram;
-    cpu;
-    ligado;
-    constructor(n, ram, cpu) {
-        this.nome = n;
-        this.ram = ram;
-        this.cpu = cpu;
-        this.ligado = false;
-        console.log(`Computador ${this.nome} criado`);
-    }
-    info() {
-        console.log(`Nome: ${this.nome}`);
-        console.log(`RAM: ${this.ram}`);
-        console.log(`CPU: ${this.cpu}`);
-        console.log(`Ligado: ${this.ligado ? "Sim" : "Não"}`);
-    }
-    ligar() {
-        this.ligado = true;
-    }
-    desligar() {
-        this.ligado = false;
-    }
-    upgradeRam(qtde) {
-        if (qtde <= 0) {
-            console.log("Não aceitamos valores inválidos.");
-            return;
-        }
-        this.ram = qtde;
+class Conta {
+    numero;
+    titular;
+    constructor(numero, titular) {
+        this.numero = numero;
+        this.titular = titular;
     }
 }
-//Instanciando o objeto
-const c1 = new Computador("Rapidão", 64, 10);
-const c2 = new Computador("Carão", 32, 5);
-const c3 = new Computador("Gamer", 128, 10);
-c1.nome = "Lenovo T430";
-c1.ligar();
-c1.upgradeRam(-10);
-c1.upgradeRam(12);
-c1.info();
+class ContaPF extends Conta {
+}
+class ContaPJ extends Conta {
+}
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,14 @@
-class Computador{
-    public nome:string
-    private ram:number
-    private cpu:number
-    protected ligado:boolean
-    constructor(n:string, ram:number, cpu:number){
-        this.nome=n
-        this.ram=ram
-        this.cpu=cpu
-        this.ligado=false
-        console.log(`Computador ${this.nome} criado`)
-    }
-    info():void{
-        console.log(`Nome: ${this.nome}`)
-        console.log(`RAM: ${this.ram}`)
-        console.log(`CPU: ${this.cpu}`)
-        console.log(`Ligado: ${this.ligado ? "Sim": "Não"}`)
-    }
-    ligar():void{
-        this.ligado=true
-    }
-    desligar():void{
-        this.ligado=false
-    }
-    upgradeRam(qtde:number):void{
-        if(qtde<=0){
-            console.log("Não aceitamos valores inválidos.")
-            return
-        }
-        this.ram=qtde
+class Conta{
+    numero:number
+    titular:String
+    constructor(numero:number, titular:string){
+        this.numero=numero
+        this.titular=titular
     }
 }
+class ContaPF extends Conta{//ContaPF herda metodos e propriedades de Conta
 
-//Instanciando o objeto
-const c1=new Computador("Rapidão", 64, 10)
-const c2=new Computador("Carão", 32, 5)
-const c3=new Computador("Gamer", 128, 10)
+}
+class ContaPJ extends Conta{//ContaPJ herda metodos e propriedades de Conta
 
-c1.nome="Lenovo T430"
-c1.ligar()
-c1.upgradeRam(-10)
-c1.upgradeRam(12)
-c1.info()
-
+}
