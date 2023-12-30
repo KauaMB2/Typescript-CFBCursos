73d211a411d16de952059a1588fd0547e1e1cf2e
KauaMB2 <i.kauamoreirabatista@gmail.com>
2023-12-30 15:41:20 -0300

    Aula21

--- build/main.js
+++ build/main.js
@@ -2,12 +2,27 @@
 class Conta {
     numero;
     titular;
-    constructor(numero, titular) {
-        this.numero = numero;
+    constructor(titular) {
+        this.numero = this.gerarNumeroConta();
         this.titular = titular;
+    }
+    gerarNumeroConta() {
+        return Math.floor(Math.random() * 100000) + 1;
     }
 }
 class ContaPF extends Conta {
+    cpf;
+    constructor(cpf, titular) {
+        super(titular); //Pasa para a classe pai
+        this.cpf = cpf;
+    }
 }
 class ContaPJ extends Conta {
+    cnpj;
+    constructor(cnpj, titular) {
+        super(titular); //Pasa para a classe pai
+        this.cnpj = cnpj;
+    }
 }
+const c1 = new ContaPJ(123456, "Abc");
+console.log(c1.numero);
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,30 @@
 class Conta{
     numero:number
     titular:String
-    constructor(numero:number, titular:string){
-        this.numero=numero
+    constructor(titular:string){
+        this.numero=this.gerarNumeroConta()
         this.titular=titular
+    }
+    gerarNumeroConta():number{
+        return Math.floor(Math.random()*100000)+1
     }
 }
 class ContaPF extends Conta{//ContaPF herda metodos e propriedades de Conta
-
+    cpf:number
+    constructor(cpf:number, titular:string){
+        super(titular)//Pasa para a classe pai
+        this.cpf=cpf
+    }
 }
 class ContaPJ extends Conta{//ContaPJ herda metodos e propriedades de Conta
-
+    cnpj:number
+    constructor(cnpj:number, titular:string){
+        super(titular)//Pasa para a classe pai
+        this.cnpj=cnpj
+    }
 }
+
+const c1= new ContaPJ(123456, "Abc")
+
+console.log(c1.numero)
+
