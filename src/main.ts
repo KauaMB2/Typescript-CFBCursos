5bb56002ad56a153c6a848e63e1b416341492ccb
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-01-03 17:46:21 -0300

    Aula37

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,6 @@
-
-let {cor1, cor2, cor3, cor4}={
-    cor1: "Verde",
-    cor2: "Vermelho",
-    cor3: "Amarelo",
-    cor4: "Azul"
+abstract class Globais{
+    public static teste:number=0
+    public static ateste:number[]=[]
 }
-console.log(cor1)
 
-let [nu1=0,nu2=0,nu3=0]=[10,20]
-console.log(nu1)
-
-let [...t]="Curso de Typescript".split(" ")
-console.log(t)
+console.log(Globais.teste)
