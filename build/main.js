8534c66026faf3e09a87728220a7964d474e0e6e
KauaMB2 <i.kauamoreirabatista@gmail.com>
2024-01-03 17:30:12 -0300

    Aula34

--- build/main.js
+++ build/main.js
@@ -1,15 +1,53 @@
 "use strict";
 var Veiculos;
 (function (Veiculos) {
+    let Cores;
+    (function (Cores) {
+        Cores[Cores["Preto"] = 0] = "Preto";
+        Cores[Cores["Branco"] = 1] = "Branco";
+        Cores[Cores["Vermelho"] = 2] = "Vermelho";
+        Cores[Cores["Amarelo"] = 3] = "Amarelo";
+        Cores[Cores["Azul"] = 4] = "Azul";
+        Cores[Cores["Prata"] = 5] = "Prata";
+    })(Cores || (Cores = {}));
     class Carro {
         nome;
         motor;
-        constructor(nome) {
+        cor;
+        constructor(nome, motor, cor) {
             this.nome = nome;
-            this.motor = new Motores.Motor(3, 10);
+            this.motor = motor;
+            this.cor = Cores[cor];
+        }
+        ligar() {
+            this.motor.liga = true;
+        }
+        desliga() {
+            this.motor.liga = false;
+        }
+        get meuNome() {
+            return this.nome;
+        }
+        get estouLigado() {
+            return this.motor.liga;
+        }
+        get pegarCor() {
+            return this.cor;
         }
     }
     Veiculos.Carro = Carro;
+    class CarroEsportivo extends Carro {
+        constructor(nome, cor) {
+            super(nome, new Motores.Motor(6, 300, new Motores.Turbo(6)), 2);
+        }
+    }
+    Veiculos.CarroEsportivo = CarroEsportivo;
+    class CarroPopular extends Carro {
+        constructor(nome, cor) {
+            super(nome, new Motores.Motor(2, 100, new Motores.Turbo(1)), 1);
+        }
+    }
+    Veiculos.CarroPopular = CarroPopular;
 })(Veiculos || (Veiculos = {}));
 var Motores;
 (function (Motores) {
@@ -41,5 +79,7 @@ var Motores;
     }
     Motores.Motor = Motor;
 })(Motores || (Motores = {}));
-const p1 = new Veiculos.Carro("Rapid");
-console.log(p1.nome);
+const p1 = new Veiculos.Carro("Rapid", new Motores.Motor(3, 10), 2);
+console.log(p1.meuNome);
+console.log(p1.estouLigado);
+console.log(p1.pegarCor);
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,39 @@
 
 namespace Veiculos{
+    enum Cores{"Preto", "Branco", "Vermelho", "Amarelo", "Azul", "Prata"}
     export class Carro{
-        nome:string
-        motor:Motores.Motor
-        constructor(nome:string){
+        private nome:string
+        private motor:Motores.Motor
+        private cor:string
+        constructor(nome:string, motor: Motores.Motor, cor:Cores){
             this.nome=nome
-            this.motor=new Motores.Motor(3,10)
+            this.motor=motor
+            this.cor=Cores[cor]
+        }
+        public ligar(){
+            this.motor.liga=true
+        }
+        public desliga(){
+            this.motor.liga=false
+        }
+        get meuNome(){
+            return this.nome
+        }
+        get estouLigado(){
+            return this.motor.liga
+        }
+        get pegarCor(){
+            return this.cor
+        }
+    }
+    export class CarroEsportivo extends Carro{
+        constructor(nome:string, cor: Cores){
+            super(nome, new Motores.Motor(6,300, new Motores.Turbo(6)), 2)
+        }
+    }
+    export class CarroPopular extends Carro{
+        constructor(nome:string, cor: Cores){
+            super(nome, new Motores.Motor(2,100, new Motores.Turbo(1)), 1)
         }
     }
 }
@@ -40,5 +68,8 @@ namespace Motores{
     }
 }
 
-const p1=new Veiculos.Carro("Rapid")
-console.log(p1.nome)
+const p1=new Veiculos.Carro("Rapid", new Motores.Motor(3,10),2)
+console.log(p1.meuNome)
+console.log(p1.estouLigado)
+console.log(p1.pegarCor)
+
